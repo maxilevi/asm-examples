d6dfd4a53f8f90acd13f03ca5a79c4324b6d7511
Zaphyk <zaphyk@live.com>
2019-11-10 03:40:53 -0300

    added packed bcd parser routine

--- format_converter.asm
+++ format_converter.asm
@@ -26,22 +26,24 @@ mensaje_inicial db "Seleccione el tipo de operacion:",10,\
                    "7 - Ingresar un numero decimal y mostrar configuracion hexadecimal del BPF C/S de 16 bits",10,\
                    "8 - Ingresar un numero decimal y mostrar configuracion binaria del BPF C/S de 16 bits",10,0
 formato_elegir_operacion db "%d"
+recibir_string_formato db "%s"
 seleccion_operacion db "Se eligio la operacion %d",10,0
 operacion_invalida db "Operacion fuera de rango, vuelva a elegir",10,0
-ingresar_operacion_bin_BCD dd "Ingrese configuracion binaria de un BCD empaquetado de 4 bytes",10,0
-mostrar_numero_formato dd "Su numero es: %d",10,0
-; Tabla para hacer conversiones
+ingresar_operacion_bin_BCD db "Ingrese configuracion binaria de un BCD empaquetado de 4 bytes",10,0
+mostrar_numero_formato db "Su numero es: %d",10,0
 
+; Tabla de potencias en base 10 para hacer conversiones
+tabla_potencias_diez dd 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000
 section .bss
 ; Reservo este espacio para representar un binario en texto
-binario_string resb 32
+binario_string resb 64
 string_largo_ptr resd 1
 string_largo resd 1
 tipo_operacion resd 1
 numero_ingresado resd 1
 binario_en_memoria resd 1
-bcd_en_memoria
-decimal_en_memoria
+bcd_en_memoria resd 1
+decimal_en_memoria resd 1
 
 section .text
 _main:
@@ -161,22 +163,54 @@ BCD_a_decimal:
     mov dword[decimal_en_memoria],0
     ; Mi idea es usar una mask de 4 bits (1111) para ir agarrando cada nibble y sumarlo al numero con su base.
     ; En cada iteracion del loop hago un shift a la derecha de la mask para pasar al otro nible
-    mov ebx, F000h
+    mov ebx, 0F0000000h
     BCD_a_decimal_loop:
         ; Cargo el numero
         mov eax,dword[bcd_en_memoria]
         ; Le aplico la mask
         and eax,ebx
         ; Hago un shift para tener el numero "real"
-        
+        ; Para esto nos fijamos el nibble que estamos viendo y lo multiplicamos por 4 (hay 4 bits en cada nibble)
+        ; Guardo el valor de ecx en la parte baja de edx
+        mov edx,0
+        mov dl,cl
+        imul ecx,4
+        shr eax,cl
+        ; Guardo cl multiplicado por 4 en la parte alta de edx para usarlo como offset despues
+        ; A lo sumo ecx es 7 -> 7 * 4 = 28 que es menor a 256 (8 bits que tiene dh)
+        mov dh,cl
+        mov ecx,0
+        mov cl,dl
+        ; Shifteo 8 bits edx para tener el valor real de dh
+        shr edx,8
+        ; Le restamos 4 a edx para agarrar siempre una potencia menos ya que el signo no lo tomamos
+        sub edx,4
         ; Lo multiplico por su potencia en base 10
-        
+        mov edx,dword[tabla_potencias_diez + edx]
+        imul eax,edx
         ; Finalmente lo añado
-        add dword[decimal],eax
+        add dword[decimal_en_memoria],eax
         ; Muevo la mask 4 bits
         shr ebx,4
     loop BCD_a_decimal_loop
-    ret
+    ; Ahora que cree el numero busco el signo que esta en el ultimo nibble
+    mov ebx,0000000Fh
+    mov eax,dword[bcd_en_memoria]
+    and eax,ebx
+    ; Ahora comparo. Si es CAFE entonces es positivo, sino es negativo
+    cmp eax,0Bh
+    je BCD_a_decimal_negativo
+    cmp eax,0Dh
+    je BCD_a_decimal_negativo
+    ; Si llegamos aca es positivo (CAFE)
+    jmp BCD_a_decimal_fin
+    BCD_a_decimal_negativo:
+        mov eax,dword[decimal_en_memoria]
+        imul eax,-1
+        mov dword[decimal_en_memoria],eax
+    ; Finalizo
+    BCD_a_decimal_fin:
+        ret
 
 ; Convierte un string en binario a un numero en la memoria
 ; Va por cada digito y lo añade a un lugar de la memoria usando el left shift
@@ -184,14 +218,26 @@ string_binario_a_numero:
     mov dword[binario_en_memoria],0
     mov ecx,0
     binario_a_numero_loop:
+        ; Copiamos el caracter en el eax
+        mov eax,0
         mov al,byte[binario_string + ecx]
         ; Si llegamos al fin del string finalizamos
         cmp al,0
         je fin_binario_a_numero_loop
         sub eax,zero_ascii
-        shl al,cl
-        add word[binario_en_memoria],ax
+        ; Guardo temporalmente el valor del ecx en el edx para poder hacer un shift
+        mov edx,ecx
+        ; Le pongo los bits que tiene el string
+        mov ecx,ebx
+        ; Le resto los que hicimos
+        sub ecx,edx
+        sub ecx,1
+        shl eax,cl
+        ; Restauro
+        mov ecx,edx
+        add dword[binario_en_memoria],eax
         inc ecx
+        ; Terminamos?
         cmp ecx,ebx
         je fin_binario_a_numero_loop
         jmp binario_a_numero_loop
@@ -231,8 +277,10 @@ calcular_largo:
         ret
 
 recibir_string_stdin:
+    mov 
     push binario_string
-    call _gets
-    add esp,4
+    push recibir_string_formato
+    call _scanf
+    add esp,8
     ret
     
